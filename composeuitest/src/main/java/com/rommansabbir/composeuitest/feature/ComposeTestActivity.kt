568b6dfcdf02052b21b7e7c71b1cc6473af8367b
rommansabbir <rommansabbir@gmail.com>
2023-05-19 23:27:41 +0600

    empty compose activity added with resources

--- composeuitest/src/main/java/com/rommansabbir/composeuitest/feature/ComposeTestActivity.kt
+++ composeuitest/src/main/java/com/rommansabbir/composeuitest/feature/ComposeTestActivity.kt
@@ -1,4 +1,4 @@
-package com.rommansabbir.androiduitestexample
+package com.rommansabbir.composeuitest.feature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rommansabbir.composeuitest.theme.AndroidUITestExampleTheme
 
-class MainActivity : ComponentActivity() {
+class ComposeTestActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
