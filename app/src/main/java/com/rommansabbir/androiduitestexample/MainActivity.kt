b7158f713f244b7eb7795c3eb6f65adabfe53516
rommansabbir <rommansabbir@gmail.com>
2023-05-19 23:38:45 +0600

    empty compose activity added with resources

--- app/src/main/java/com/rommansabbir/androiduitestexample/MainActivity.kt
+++ app/src/main/java/com/rommansabbir/androiduitestexample/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.rommansabbir.composeuitest.theme.AndroidUITestExampleTheme
+import com.rommansabbir.androiduitestexample.ui.theme.AndroidUITestExampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidUITestExampleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidUITestExampleTheme {
-        Greeting("Android")
     }
 }
